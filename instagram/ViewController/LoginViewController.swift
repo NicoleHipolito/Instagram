b72ca1528fe151077265ee8161399d6ccf1b9985
NicoleHipolito <nhipolito1@gmail.com>
2018-02-25 19:56:20 -0800

    Finished application

--- instagram/ViewController/LoginViewController.swift
+++ instagram/ViewController/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
